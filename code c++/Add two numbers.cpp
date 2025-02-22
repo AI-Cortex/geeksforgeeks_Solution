f73f8013587a46c513b35ec6603da6d57d4e2b0e
AI Cortex <artificial.intelligence.cortex@gmail.com>
2025-02-22 20:33:52 +0330

    add code
    - Add two numbers

---
+++ code c++/Add two numbers.cpp
@@ -0,0 +1,10 @@
+// my profile : https://www.geeksforgeeks.org/user/aicortex/
+// Geeksforgeeks solution github repository : https://github.com/AI-Cortex/geeksforgeeks_Solution
+//////////////////////////////////////////////////////////////////////
+// Name: Add two numbers
+// Link: https://www.geeksforgeeks.org/problems/add-two-numbers/1
+
+auto add = [](int x, int y)
+{
+    return x + y;
+};

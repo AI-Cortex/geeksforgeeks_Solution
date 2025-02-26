9581f78c8c58ba59dc4c31337c3f557a84d1c254
AI Cortex <artificial.intelligence.cortex@gmail.com>
2025-02-26 10:04:04 +0330

    add code
    - Length of The List

---
+++ code python/Length of The List.py
@@ -0,0 +1,9 @@
+# my profile : https://www.geeksforgeeks.org/user/aicortex/
+# Geeksforgeeks solution github repository : https://github.com/AI-Cortex/geeksforgeeks_Solution
+# //////////////////////////////////////////////////////////////////////
+# name : Length of The List
+# link : https://www.geeksforgeeks.org/problems/length-of-the-list/1
+
+
+def listLength(arr):
+    return len(arr)

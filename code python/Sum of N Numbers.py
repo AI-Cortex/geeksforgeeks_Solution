f4a099d3ed5d2120932d362b1eb6d0e1d0eb60d3
AI Cortex <artificial.intelligence.cortex@gmail.com>
2025-04-23 06:09:23 +0330

    code add
    - Sum of N Numbers

---
+++ code python/Sum of N Numbers.py
@@ -0,0 +1,9 @@
+# // my profile : https://www.geeksforgeeks.org/user/aicortex/
+# // Geeksforgeeks solution github repository : https://github.com/AI-Cortex/geeksforgeeks_Solution
+# //////////////////////////////////////////////////////////////////////
+# // name : Sum of N Numbers
+# // link : https://www.geeksforgeeks.org/problems/sum-of-n-number--123033/1
+
+n = int(input())
+# Your code here
+print(n*(n+1)//2)
